e50b95e52c32d4503d5b9efa2b38e5e942afc22a
Matheus Caetano <m243501@dac.unicamp.br>
2024-09-07 12:10:31 -0300

    Começando :)

---
+++ ball.gd
@@ -0,0 +1,17 @@
+extends Sprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+@export var speed = 100
+@export var centro : Node2D
+@export var raio = 100
+
+func _ready():
+	position = centro.position + Vector2(cos(3 * PI/2), sin(3 * PI/2)) * raio
+	pass # Replace with function body.
+
+
+func _physics_process(delta):
+	
+	pass
+	
