604f22af8a675f93ee4638a21a681e0bb9d90127
Matheus Caetano <m243501@dac.unicamp.br>
2024-09-07 13:40:45 -0300

    geracao de prompt de clique

--- ball.gd
+++ ball.gd
@@ -1,17 +1,15 @@
 extends Sprite2D
 
-
 # Called when the node enters the scene tree for the first time.
 @export var speed = 100.0
-@export var centro : Node2D
-@export var raio = 100
+@export var cd : Node2D
 
 func _ready():
-	position = centro.position + Vector2(cos(3 * PI/2), sin(3 * PI/2)) * raio
+	position = Vector2(cos(3 * PI/2), sin(3 * PI/2)) * cd.radius
 	pass # Replace with function body.
 
 
 func _physics_process(delta):
-	position = centro.position + (position - centro.position).rotated(speed * delta)
+	position = position.rotated(speed * delta)
 	pass
 	
--- cd.gd
+++ cd.gd
@@ -1,10 +1,21 @@
 extends Sprite2D
 
 @export var pattern : PackedFloat32Array
-@export var prompt_object : Node2D
+@export var radius = 100.0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var prompt_base = preload("res://press_prompt.tscn")
+	
+	for element in pattern:
+		print(position)
+		var angle_converted = 2 * PI * element - PI/2
+		var pos = Vector2(cos(angle_converted), sin(angle_converted)) * radius
+		var prompt_obj = prompt_base.instantiate()
+		prompt_obj.position = pos
+		add_child(prompt_obj)
+		print(prompt_obj.position)
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
