853f217e43abf3fd153e42dcd7dc8a525fd5b5cd
Tiago Dall'Oca <tdalloca@gmail.com>
2024-09-08 15:24:47 -0300

    melhorias e melhorias no menu

--- AnimatedSprite2D.gd
+++ AnimatedSprite2D.gd
@@ -1,15 +1,12 @@
-extends Node2D
+extends AnimatedSprite2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	play("default")
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-
-func _on_button_begin_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
